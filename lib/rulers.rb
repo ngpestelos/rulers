bc75eda71ccf0a8172b7fcae0bac2a9a8b131b3f
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-07-28 17:13:22 +0800

    add Rack::Request and Rack::Response

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -8,13 +8,23 @@ require "rulers/file_model"
 module Rulers
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       if (controller.class == Controller)
         [302, {'Location' => '/quotes/a_quote'}, []]
       else
         text = controller.send(act)
-        [200, {'Content-Type' => 'text/html'}, [text]]
+        if controller.get_response
+          st, hd, rs = controller.get_response.to_a
+          [st, hd, [rs.body].flatten]
+        else
+          [200, {'Content-Type' => 'text/html'}, [text]]
+        end
       end
     end
   end
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -5,6 +5,28 @@ module Rulers
   class Controller
     include Rulers::Model
 
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def response(text, status = 200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response
+      @response
+    end
+
+    def render_response(*args)
+      response(render(*args))
+    end
+
+    def params
+      request.params
+    end
+
     def initialize(env)
       @env = env
     end
